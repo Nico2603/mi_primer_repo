45a1e0de078ba8114793c272134756b7e4e21087
Nicolás Ceballos Brito <nicolasceballosbrito@gmail.com>
2022-11-01 16:49:52 -0500

    Cambio de título

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -7,15 +7,17 @@ export const FirstPage = () => {
         setValor(Math.max(valor+num, 0)) 
     }
 
-    
+    const reset = () =>{
+        setValor(10)
+    }
 
     return (
         <>
-        <h1>Contador</h1>
+        <h1>Contador Mejorado</h1>
         <h1>{valor}</h1>
         <button onClick={() => modificarEstado(+1)}> +1 </button>
         <button onClick={() => modificarEstado(-1)}> -1 </button> <br />
-        <button onClick={() => modificarEstado(10)}> Reset </button> <br />
+        <button onClick={reset}> Reset </button> <br />
 
         </>
     )
